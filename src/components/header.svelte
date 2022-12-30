23d25d91ac171c1cb0d8498e45ff74f083edc814
Mukul Kumar Gupta <76431593+codemukul@users.noreply.github.com>
2022-12-30 18:46:59 +0000

    header

---
+++ src/components/header.svelte
@@ -0,0 +1,105 @@
+<script>
+</script>
+
+<header>
+  <div class="frame1">
+    <div class="frame5">
+      <svg
+        class="svg"
+        width="29"
+        height="29"
+        viewBox="0 0 29 29"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M27.7083 0.5L27.455 0.5475L19 3.825L9.5 0.5L0.57 3.50833C0.2375 3.61917 0 3.90417 0 4.26833V28.2083C0 28.6517 0.348333 29 0.791667 29L1.045 28.9525L9.5 25.675L19 29L27.93 25.9917C28.2625 25.8808 28.5 25.5958 28.5 25.2317V1.29167C28.5 0.848333 28.1517 0.5 27.7083 0.5ZM11.0833 4.41083L17.4167 6.6275V25.0892L11.0833 22.8725V4.41083ZM3.16667 5.97833L7.91667 4.37917V22.9042L3.16667 24.7408V5.97833ZM25.3333 23.5217L20.5833 25.1208V6.61167L25.3333 4.775V23.5217Z"
+          fill="#8447FF"
+        />
+      </svg>
+    </div>
+    <span class="text">Entries</span>
+  </div>
+  <img class="logo" src="/logo.jpg" alt="logo" />
+</header>
+
+<style>
+  header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-end;
+    padding: 12px 32px;
+    gap: 266px;
+
+    position: absolute;
+    width: 1280px;
+    height: 64px;
+    left: 0px;
+    top: 0px;
+
+    border: 1px solid #d0d5dd;
+  }
+
+  .logo {
+    width: 40px;
+    height: 40px;
+    border-radius: 100px;
+
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+  }
+
+  .frame1 {
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+    padding: 0px;
+    gap: 8px;
+
+    width: 147.5px;
+    height: 39px;
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+  }
+
+  .frame5 {
+    width: 28.5px;
+    height: 28.5px;
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+  }
+
+  .text {
+    width: 111px;
+    height: 39px;
+
+    /* Headline 2 */
+
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 32px;
+    line-height: 39px;
+    /* identical to box height */
+
+    display: flex;
+    align-items: flex-end;
+    letter-spacing: 0.384px;
+
+    /* Primary */
+
+    color: #8447ff;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+  }
+</style>
